8e183ee9f21ce2b044b5eae37b2f9f3f8ae8bb1c
Shayan Haque <shyyawn@gmail.com>
2021-10-13 13:37:53 +0800

    Add mysql connection for data stores

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/gin-gonic/gin v1.7.2
 	github.com/go-playground/validator/v10 v10.6.1
 	github.com/go-redis/redis/v8 v8.10.0
+	github.com/go-sql-driver/mysql v1.6.0
 	github.com/gocql/gocql v0.0.0-20201215165327-e49edf966d90
 	github.com/linkedin/goavro/v2 v2.10.0
 	github.com/logrusorgru/aurora/v3 v3.0.0
--- x/data_store/mysql.go
+++ x/data_store/mysql.go
@@ -1,13 +1,52 @@
 package data_store
 
 import (
+	"database/sql"
+	"github.com/go-sql-driver/mysql"
 	"github.com/shyyawn/go-to/x/source"
 	"github.com/spf13/viper"
+	"log"
+	"time"
 )
 
 type Mysql struct {
+	db       *sql.DB
+	User     string `mapstructure:"user"`
+	Password string `mapstructure:"password"`
+	Net      string `mapstructure:"net"`
+	Addr     string `mapstructure:"addr"`
+	DBName   string `mapstructure:"db_name"`
 }
 
 func (ds *Mysql) LoadFromConfig(key string, config *viper.Viper) error {
 	return source.LoadFromConfig(key, config, ds)
+}
+
+func (ds *Mysql) Db() *sql.DB {
+
+	// if the db object is already created
+	if ds.db != nil {
+		// check ping
+		if pingErr := ds.db.Ping(); pingErr == nil {
+			return ds.db
+		}
+	}
+
+	cfg := mysql.Config{
+		User:         ds.User,
+		Passwd:       ds.Password,
+		Net:          ds.Net,
+		Addr:         ds.Addr,
+		DBName:       ds.DBName,
+		Timeout:      5 * time.Second,
+		ReadTimeout:  60 * time.Second,
+		WriteTimeout: 60 * time.Second,
+	}
+
+	var err error
+	ds.db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return ds.db
 }
