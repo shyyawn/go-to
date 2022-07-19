390bc03eae0933d26c24dba14cd2836f21b229ba
Shayan <shyyawn@gmail.com>
2022-07-19 23:16:27 +0500

    Adding Gorm

--- go.mod
+++ go.mod
@@ -17,6 +17,8 @@ require (
 	github.com/riferrei/srclient v0.5.0
 	github.com/spf13/viper v1.10.1
 	google.golang.org/api v0.70.0
+	gorm.io/driver/mysql v1.3.5
+	gorm.io/gorm v1.23.8
 )
 
 require (
@@ -49,6 +51,8 @@ require (
 	github.com/jcmturner/gofork v1.0.0 // indirect
 	github.com/jcmturner/gokrb5/v8 v8.4.2 // indirect
 	github.com/jcmturner/rpc/v2 v2.0.3 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.14.4 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
---
+++ x/data_store/gorm.go
@@ -0,0 +1,55 @@
+package data_store
+
+import (
+	"log"
+
+	"github.com/shyyawn/go-to/x/source"
+	"github.com/spf13/viper"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type Gorm struct {
+	User                 string `mapstructure:"user"`
+	Password             string `mapstructure:"password"`
+	Net                  string `mapstructure:"net"`
+	Addr                 string `mapstructure:"addr"`
+	DBName               string `mapstructure:"db_name"`
+	AllowNativePasswords bool   `mapstructure:"allow_native_passwords"`
+	Timeout              int    `json:"timeout"`
+	ReadTimeout          int    `json:"read_timeout"`
+	WriteTimeout         int    `json:"write_timeout"`
+}
+
+func (ds *Gorm) LoadFromConfig(key string, config *viper.Viper) error {
+	err := source.LoadFromConfig(key, config, ds)
+	//Defaults
+	if ds.Timeout == 0 {
+		ds.Timeout = 5
+	}
+	if ds.ReadTimeout == 0 {
+		ds.ReadTimeout = 60
+	}
+	if ds.WriteTimeout == 0 {
+		ds.WriteTimeout = 60
+	}
+	return err
+}
+
+func (ds *Gorm) Db() *gorm.DB {
+	dsn := ds.User + ":" + ds.Password + "@tcp(" + ds.Addr + ")/" + ds.DBName + "?charset=utf8&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.New(mysql.Config{
+		DSN:                       dsn,   // data source name
+		DefaultStringSize:         256,   // default size for string fields
+		DisableDatetimePrecision:  true,  // disable datetime precision, which not supported before MySQL 5.6
+		DontSupportRenameIndex:    true,  // drop & create when rename index, rename index not supported before MySQL 5.7, MariaDB
+		DontSupportRenameColumn:   true,  // `change` when rename column, rename column not supported before MySQL 8, MariaDB
+		SkipInitializeWithVersion: false, // auto configure based on currently MySQL version
+	}), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return db
+}
