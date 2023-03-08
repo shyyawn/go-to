85ea5928406d797933dab9361c16dcb32a005f6c
Shayan Haque <shyyawn@gmail.com>
2023-03-08 10:10:18 +0800

    Adding connections defaults for mysql

--- x/data_store/mysql.go
+++ x/data_store/mysql.go
@@ -21,6 +21,8 @@ type Mysql struct {
 	Timeout              int    `json:"timeout"`
 	ReadTimeout          int    `json:"read_timeout"`
 	WriteTimeout         int    `json:"write_timeout"`
+	MaxOpenConns         int    `json:"max_open_conns"`
+	MaxIdleConns         int    `json:"max_idle_conns"`
 	lock                 sync.RWMutex
 }
 
@@ -35,6 +37,12 @@ func (ds *Mysql) LoadFromConfig(key string, config *viper.Viper) error {
 	}
 	if ds.WriteTimeout == 0 {
 		ds.WriteTimeout = 60
+	}
+	if ds.MaxOpenConns == 0 {
+		ds.MaxOpenConns = 5
+	}
+	if ds.MaxIdleConns == 0 {
+		ds.MaxIdleConns = 5
 	}
 	return err
 }
@@ -67,6 +75,12 @@ func (ds *Mysql) Db() *sql.DB {
 	ds.db, err = sql.Open("mysql", cfg.FormatDSN())
 	if err != nil {
 		log.Fatalln(err)
+	}
+	if ds.MaxOpenConns != 0 {
+		ds.db.SetMaxOpenConns(ds.MaxOpenConns)
+	}
+	if ds.MaxIdleConns != 0 {
+		ds.db.SetMaxIdleConns(ds.MaxIdleConns)
 	}
 	return ds.db
 }
